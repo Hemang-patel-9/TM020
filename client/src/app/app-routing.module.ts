0b0a57c59378bd2d85316d3fa949cdc97dda45b1
Hemang Patel <hemang9705@gmail.com>
2024-03-02 14:00:48 +0530

    hello

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { RoughComponent } from './rough/rough.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HeroSectionComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'ss', component: RoughComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
